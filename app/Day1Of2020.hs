c7055df82caca30234f1984742399089f540014b
Even Brenden <evenbrenden@gmail.com>
2021-11-29 10:21:48 +0100

    Solve 2020 day 1

---
+++ app/Day1Of2020.hs
@@ -0,0 +1,47 @@
+-- https://adventofcode.com/2020/day/1
+
+module Day1Of2020 where
+
+import Control.Monad
+import System.IO
+import Text.Trifecta
+
+int :: Parser Int
+int = fromIntegral <$> integer
+
+allInts :: Parser [Int]
+allInts = do
+    is <- many int
+    eof
+    return is
+
+parseAllInts :: String -> Result [Int]
+parseAllInts = parseString allInts mempty
+
+handleResult (Success is) = is
+handleResult (Failure f) = error $ show f
+
+parse :: String -> [Int]
+parse = handleResult . parseAllInts
+
+part1 :: [Int] -> Int
+part1 ints = head $ do
+    x <- ints
+    y <- ints
+    guard $ x + y == 2020
+    return $ x * y
+
+part2 :: [Int] -> Int
+part2 ints = head $ do
+    x <- ints
+    y <- ints
+    z <- ints
+    guard $ x + y + z == 2020
+    return $ x * y * z
+
+solve :: IO ()
+solve = do
+    handle <- openFile "app/Day1Of2020.txt" ReadMode
+    contents <- hGetContents handle
+    print $ part1 . parse $ contents
+    print $ part2 . parse $ contents
--- app/Main.hs
+++ app/Main.hs
@@ -2,32 +2,7 @@
 
 module Main where
 
-import System.IO
-import Text.Trifecta
-
-int :: Parser Int
-int = fromIntegral <$> integer
-
-allInts :: Parser [Int]
-allInts = do
-    is <- many int
-    eof
-    return is
-
-parseAllInts :: String -> Result [Int]
-parseAllInts = parseString allInts mempty
-
-handleResult (Success is) = is
-handleResult (Failure f) = error $ show f
-
-parse :: String -> [Int]
-parse = handleResult . parseAllInts
-
-solve :: [Int] -> [Int]
-solve = id
+import Day1Of2020
 
 main :: IO ()
-main = do
-    handle <- openFile "input.txt" ReadMode
-    contents <- hGetContents handle
-    print $ solve . parse $ contents
+main = solve
