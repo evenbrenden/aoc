d6d6450bf83550b978a60ff6dda2331730a463cc
Even Brenden <evenbrenden@gmail.com>
2021-12-04 11:08:26 +0100

    Refactor

--- 2021-12-03.hs
+++ 2021-12-03.hs
@@ -8,6 +8,11 @@ import Control.Applicative
 import Data.List
 import Text.Trifecta
 
+type Number = [Bool]
+type Numbers = [Number]
+type Comparator = Number -> Bool
+type Filter = Numbers -> Numbers
+
 parseTrue :: Parser Bool
 parseTrue = do
     char '1'
@@ -21,19 +26,19 @@ parseFalse = do
 parseBool :: Parser Bool
 parseBool = parseTrue <|> parseFalse
 
-parseNumber:: Parser [Bool]
+parseNumber:: Parser Number
 parseNumber = do
     bits <- some parseBool
     char '\n'
     return bits
 
-parseNumbers :: Parser [[Bool]]
+parseNumbers :: Parser Numbers
 parseNumbers = do
     bitsList <- many parseNumber
     eof
     return bitsList
 
-mostCommon :: [Bool] -> Bool
+mostCommon :: Number -> Bool
 mostCommon bools =
     let numTrues = length . filter (==True) $ bools
         numFalses = length . filter (==False) $ bools
@@ -43,46 +48,48 @@ pow2 :: Bool -> Int -> Int
 pow2 True = (^) 2
 pow2 False = const 0
 
-int :: [Bool] -> Int
+int :: Number -> Int
 int num = sum $ uncurry pow2 <$> zip (reverse num) [0..]
 
-gamma :: [[Bool]] -> [Bool]
+gamma :: Numbers -> Number
 gamma = fmap mostCommon . transpose
 
-epsilon :: [[Bool]] -> [Bool]
+epsilon :: Numbers -> Number
 epsilon = fmap not . gamma
 
-part1 :: [[Bool]] -> Int
+part1 :: Numbers -> Int
 part1 nums = (int . gamma) nums * (int . epsilon) nums
 
 notIf :: Bool -> (Bool -> Bool)
 notIf True = id
 notIf False = not
 
-ratingFilter :: ([Bool] -> Bool) -> [[Bool]] -> Int -> [[Bool]]
-ratingFilter _ nums _ | length nums == 1 = nums
-ratingFilter comp nums idx =
-    let cols   = transpose nums
-        common = comp $ cols !! idx
-        predi  = notIf common . (!! idx)
-    in  ratingFilter comp (filter predi nums) (idx + 1)
+ratingFilter :: Comparator -> Numbers -> Numbers
+ratingFilter comp nums =
+    let go xs _ | length xs == 1 = xs
+        go xs idx =
+            let columns   = transpose xs
+                selected  = comp (columns !! idx)
+                predicate = notIf selected . (!! idx)
+            in  go (filter predicate xs) (idx + 1)
+    in  go nums 0
 
-ogrFilter :: [[Bool]] -> Int -> [[Bool]]
+ogrFilter :: Numbers -> Numbers
 ogrFilter = ratingFilter mostCommon
 
-csrFilter :: [[Bool]] -> Int -> [[Bool]]
+csrFilter :: Numbers -> Numbers
 csrFilter = ratingFilter $ not . mostCommon
 
-rating :: ([[Bool]] -> Int -> [[Bool]]) -> [[Bool]] -> Int
-rating filt nums = int . head $ filt nums 0
+rating :: Filter -> Numbers -> Int
+rating filt nums = int . head $ filt nums
 
-ogr :: [[Bool]] -> Int
+ogr :: Numbers -> Int
 ogr = rating ogrFilter
 
-csr :: [[Bool]] -> Int
+csr :: Numbers -> Int
 csr = rating csrFilter
 
-part2 :: [[Bool]] -> Int
+part2 :: Numbers -> Int
 part2 nums = ogr nums * csr nums
 
 main :: IO ()
