5e7f650503511f861df2c1236f2bd61d460ab74a
Even Brenden <evenbrenden@gmail.com>
2021-12-04 21:06:39 +0100

    Refactor

--- 2021-12-04.hs
+++ 2021-12-04.hs
@@ -70,71 +70,49 @@ sumNumbers :: [Number] -> Int
 sumNumbers (Number n _:ns) = n + sumNumbers ns
 sumNumbers [] = 0
 
-sumUnmarked :: Board -> Int
-sumUnmarked board =
+sumBoard :: Board -> Int
+sumBoard board =
     let isUnmarked (Number _ m) = not m
         unmarkedRow = filter isUnmarked
-        sumUnmarkedRow = sumNumbers . unmarkedRow
-    in  sum $ sumUnmarkedRow <$> board
+        sumBoardRow = sumNumbers . unmarkedRow
+    in  sum $ sumBoardRow <$> board
 
 won :: (Int, Board, Bool) -> Bool
 won (_, _, w) = w
 
-inform :: a -> Board -> (a, Board, Bool)
+inform :: Int -> Board -> (Int, Board, Bool)
 inform call board = (call, board, check board)
 
-go :: [Int] -> [Board] -> (Int, Board, Bool)
-go (call:calls) boards =
-    let marked  = markBoards call boards
-        checked = inform call <$> marked
-        wins    = filter won checked
-    in
-        if length wins > 0 then
-            head wins
-        else
-            go calls marked
-go [] _ = error "no winners"
+removeWon :: (Int, Board, Bool) -> (Int, Board)
+removeWon (c, b, _) = (c, b)
 
-findWinner :: [Int] -> [Board] -> (Int, Board)
-findWinner calls boards =
-    let (call, board, _) = go calls boards
-    in  (call, board)
+findWinners :: [Int] -> [Board] -> [(Int, Board)]
+findWinners calls boards =
+    let go (call:calls') boards' wins =
+            let marked   = markBoards call boards'
+                checked  = inform call <$> marked
+                nextWins = filter won checked
+                stillNot = bored <$> filter (not . won) checked
+            in
+                go calls' stillNot (wins <> nextWins)
+        go [] _ wins = wins
+    in removeWon <$> go calls boards []
 
 score :: Int -> Board -> Int
-score x b = x * sumUnmarked b
+score x b = x * sumBoard b
 
 part1 :: ([Int], [[[Int]]]) -> Int
 part1 (calls, boards) =
     let boards' = initBoard <$> boards
-        (call, board) = findWinner calls boards'
-    in  score call board
+    in  uncurry score $ head $ findWinners calls boards'
 
 bored :: (Int, Board, Bool) -> Board
 bored (_, b, _) = b
 
-og :: [Int] -> [Board] -> [(Int, Board, Bool)] -> (Int, Board, Bool)
-og (call:calls) boards lastWins =
-    let marked   = markBoards call boards
-        checked  = inform call <$> marked
-        nextWins = filter won checked
-        stillNot = bored <$> filter (not . won) checked
-    in
-        if length nextWins > 0 then
-            og calls stillNot nextWins
-        else
-            og calls stillNot lastWins
-og [] _ wins = last wins
-
-findLastWinner :: [Int] -> [Board] -> (Int, Board)
-findLastWinner calls boards =
-    let (call, board, _) = og calls boards []
-    in  (call, board)
-
 part2 :: ([Int], [[[Int]]]) -> Int
 part2 (calls, boards) =
     let boards' = initBoard <$> boards
-        (call, board) = findLastWinner calls boards'
-    in  score call board
+    in uncurry score $ last $ findWinners calls boards'
 
 main :: IO ()
 main = do
